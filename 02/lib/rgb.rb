0e298cad3f185a12fd0923ab85b3f36ab01b119b
福井陽登 <fukuiharuto@fukuiakiratounoMacBook-Air.local>
2025-03-06 14:39:30 +0900

    to_hexメソッドをリファクタリング

--- 02/lib/rgb.rb
+++ 02/lib/rgb.rb
@@ -1,6 +1,6 @@
 def to_hex(r, g, b)
-  '#' +
-    r.to_s(16).rjust(2, '0') +
-    g.to_s(16).rjust(2, '0') +
-    b.to_s(16).rjust(2, '0')
+  #初期値として#が入る
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
+  end
 end
