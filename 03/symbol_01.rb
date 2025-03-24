98746318d2d4d64d3765334c5398fdcfee53bd1e
福井陽登 <fukuiharuto@fukuiakiratounoMacBook-Air.local>
2025-03-24 20:00:49 +0900

    シンボル基礎

---
+++ 03/symbol_01.rb
@@ -0,0 +1,11 @@
+# currencies = { 'japan' => 'yen', 'us' => 'doller', 'india' => 'rupee' }
+# p currencies['japan']
+
+# currencies = { :japan => 'yen', :us => 'dollar', :india => 'rupee' }
+# p currencies[:japan]
+
+# currencies = { japna: 'yen', us: 'dollar', india: 'rupee'}
+# p currencies[:us]
+
+currencies = { japan: :yen, us: :dollar}
+p currencies[:japan]
